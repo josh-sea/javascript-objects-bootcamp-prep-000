dd2d9b76e0941a839b54b6f5362d169f3590360c
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-09-02 19:44:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: "songTitle"};
 function updatePlayList(playList, artistName, songTitle) {
-  playList.artistName=songTitle
+  playList[`${artistName}`]=songTitle
   return playList
 }
 
